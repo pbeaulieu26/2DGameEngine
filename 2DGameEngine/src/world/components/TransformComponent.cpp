4684da191c3874d9138cc0b6e77640fd82bcad89
Patrick Beaulieu <patrick.beaulieu3@usherbrooke.ca>
2019-05-16 21:52:06 -0400

    Vector2d and transform component

--- 2DGameEngine/src/world/components/TransformComponent.cpp
+++ 2DGameEngine/src/world/components/TransformComponent.cpp
@@ -5,6 +5,17 @@
 namespace Engine {
 
     TransformComponent::TransformComponent()
+        : m_position()
+    {
+    }
+
+    TransformComponent::TransformComponent(int xPos, int yPos, double rotation)
+        : m_position(xPos, yPos), m_rotation(rotation)
+    {
+    }
+
+    TransformComponent::TransformComponent(const Vector2d<int> position, double rotation)
+        : m_position(position), m_rotation(rotation)
     {
     }
 
--- 2DGameEngine/src/world/components/TransformComponent.h
+++ 2DGameEngine/src/world/components/TransformComponent.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "BaseComponent.h"
+#include "utils/Vector2d.h"
 
 
 namespace Engine {
@@ -9,10 +10,27 @@ namespace Engine {
     {
 
     public:
+
         TransformComponent();
+        TransformComponent(int xPos, int yPos, double rotation);
+        TransformComponent(const Vector2d<int> position, double rotation);
         ~TransformComponent();
 
+        inline Vector2d<int> getPosition() const { return m_position; }
+        inline double getRotation() const { return m_rotation; }
+
+        inline void moveTo(const Vector2d<int> position) { m_position = position; }
+        inline void moveToX(const int x) { m_position.x = x; }
+        inline void moveToY(const int y) { m_position.y = y; } 
+
+        inline void setRotation(double rotation) { m_rotation = rotation; }
+
         COMPONENT_INFO("TransformComponent")
+
+    private:
+
+        Vector2d<int> m_position;
+        double m_rotation;
 
     };
 
